4821de9e81adcec1c6bd419d52363b5f3cc8f2c7
manuelfmerino <manuel.fernandez@crg.eu>
2026-05-20 12:54:37 +0200

    Included function to train CNN on full dataset after k-fold CV

--- functions/cnn_helpers.py
+++ functions/cnn_helpers.py
@@ -367,6 +367,136 @@ def kfold_splits(
     return
 
 
+def final_split(src_folder, dst_folder, test_size=0.2, n_splits=5, seed=42):
+    """
+    Creates train and test splits for final evaluation after k-fold CV.
+    Saves raw and normalized (z-score) images as .PNG in dst_folder}/fold_i/{train,validation,test}/transcription_state.
+
+    Parameters
+    ----------
+    src_folder: str
+        Path to folder containing entire dataset.
+    dst_folder: str
+        Path to folder where splits will be saved.
+    test_size: float
+        Fraction of dataset included in test set.
+    n_splits: int
+        Number of splits in k-fold cross-valudation
+
+    Returns
+    -------
+
+    """
+
+    random.seed(seed)
+    np.random.seed(seed)
+    shutil.rmtree(dst_folder, ignore_errors=True)
+
+    # Get labels and assign indices
+    classes = [
+        d for d in os.listdir(src_folder) if os.path.isdir(os.path.join(src_folder, d))
+    ]
+    classes = sorted(classes, reverse=True)
+    class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
+
+    # Get paths of all files and corresponding labels
+    filepaths = []
+    labels = []
+    for cls in classes:
+        cls_path = os.path.join(src_folder, cls)
+        for fname in os.listdir(cls_path):
+            if fname.lower().endswith(".tsv"):
+                filepaths.append(os.path.join(cls_path, fname))
+                labels.append(class_to_idx[cls])
+
+    filepaths = np.array(filepaths)
+    labels = np.array(labels)
+
+    # Generate splits
+    X_train, X_test, y_train, y_test = train_test_split(
+        filepaths,
+        labels,
+        test_size=test_size,
+        stratify=labels,
+        random_state=seed,
+    )
+
+    return_folder = []
+
+    ###############
+    # To achieve a balanced training set:
+    X0 = X_train[y_train == 0]
+    X1 = X_train[y_train == 1]
+
+    # downsample majority class
+    X0_down = resample(X0, replace=False, n_samples=len(X1), random_state=42)
+
+    # combine
+    X_train = np.concatenate([X0_down, X1]).tolist()
+    y_train = np.concatenate(
+        [np.zeros(len(X1), dtype=int), np.ones(len(X1), dtype=int)]
+    ).tolist()
+    ###############
+
+    print(f"Train size: {len(X_train)}, Test size: {len(X_test)}")
+
+    # Read training set to calculate mean and standard deviation for normalization
+    training_images = []
+    for filepath_i in X_train:
+        image_i = np.array(pd.read_csv(filepath_i, sep="\t", header=None))
+        training_images.append(image_i)
+    training_images = np.dstack(training_images)
+
+    mean_train = np.mean(training_images)
+    std_train = np.std(training_images)
+
+    # Save split raw and normalized files
+    fold_dir_raw = dst_folder + "_raw"
+    os.makedirs(fold_dir_raw, exist_ok=True)
+    fold_dir_norm = dst_folder + "_zscore"
+    os.makedirs(fold_dir_norm, exist_ok=True)
+
+    for set_i, filepaths_phase_i, labels_phase_i in [
+        ("train", X_train, y_train),
+        ("test", X_test, y_test),
+    ]:  # Iterate through all three datasets
+        for cls in classes:
+            os.makedirs(os.path.join(fold_dir_raw, set_i, cls), exist_ok=True)
+            os.makedirs(os.path.join(fold_dir_norm, set_i, cls), exist_ok=True)
+        for filepath_i, label_i in zip(
+            filepaths_phase_i, labels_phase_i
+        ):  # Iterate through each image
+            # Open .tsv file
+            image_i = np.array(pd.read_csv(filepath_i, sep="\t", header=None))
+
+            # Raw images - save as .PNG
+            dst_path_raw = os.path.join(
+                fold_dir_raw,
+                set_i,
+                classes[label_i],
+                os.path.basename(filepath_i.split("/")[-1].split(".")[0] + ".png"),
+            )
+            image_i_raw = image_i / image_i.max() * 255
+            image_i_raw = image_i_raw.astype(np.uint8)
+            Image.fromarray(image_i_raw).save(dst_path_raw)
+
+            # Normalized images - save as .tsv, as .PNG would require clipping negative values
+            dst_path_norm = os.path.join(
+                fold_dir_norm,
+                set_i,
+                classes[label_i],
+                os.path.basename(filepath_i),
+            )
+
+            image_i_norm = (image_i - mean_train) / std_train
+            image_i_norm = pd.DataFrame(image_i_norm).to_csv(
+                dst_path_norm, sep="\t", header=False, index=False
+            )
+    print(f"Created splits under '{dst_folder}'.")
+
+    return
+
+
 # Define functions
 def kfold_splits_unbalanced(
     src_folder, dst_folder, test_size=0.2, val_size=0.25, n_splits=5, seed=42
